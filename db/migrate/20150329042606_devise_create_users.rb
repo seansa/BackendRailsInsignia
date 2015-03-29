a32bf1a5332c4b35acea3a244c93ab3a78e9d6f7
Sebastian Salvatore <sebastiansalvatore@gmail.com>
2015-03-29 01:47:23 -0300

    Barra de navegacion movida a un parcial

--- db/migrate/20150329042606_devise_create_users.rb
+++ db/migrate/20150329042606_devise_create_users.rb
@@ -30,6 +30,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :name
+      t.string :permission_level
 
       t.timestamps
     end
