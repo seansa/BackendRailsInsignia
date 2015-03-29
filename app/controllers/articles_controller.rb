e40f302396040d79422467958c45516b47738748
Sebastian Salvatore <sebastiansalvatore@gmail.com>
2015-03-28 22:57:39 -0300

    Validaciones agregadas al crear aticulo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,11 +2,22 @@ class ArticlesController < ApplicationController
 	def index
 		@articles = Article.all
 	end
+
 	def show
-		@article = Articles.find(params[:id])
+		@article = Article.find(params[:id])
 	end
 
 	def new
 		@article = Article.new
 	end
+
+	def create
+		@article = Article.new(title: params[:article][:title], body: params[:article][:body])
+		if @article.save
+			redirect_to @article
+		else
+			render :new
+		end
+	end
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ActiveRecord::Base
+ 	validates :title, presence: true, length: { minimum: 5 }, uniqueness: true
+	validates :body, presence: true, length: { minimum: 20 }, uniqueness: true
 end
