d07747cdfdf69fb18ecd752fb037f8030372deb5
Sebastian Salvatore <sebastiansalvatore@gmail.com>
2015-03-29 01:27:14 -0300

    devise agregado

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
 	def create
 		@article = Article.new(article_params)
 		if @article.save
@@ -24,6 +28,15 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 		@article.destroy
 		redirect_to articles_path
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render :edit
+		end
 	end
 
 	protected
