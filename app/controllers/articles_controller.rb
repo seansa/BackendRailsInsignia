50d2cc7119cd4a44cf5d8a30a894fb24173eebdd
Sebastian Salvatore <sebastiansalvatore@gmail.com>
2015-03-29 02:50:43 -0300

    Se agrega 'Escrito por:'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		@article = Article.new(article_params)
+		@article = current_user.articles.new(article_params)
 		if @article.save
 			redirect_to @article
 		else
