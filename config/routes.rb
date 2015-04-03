46c3539a902bbbb3ad545b399c6f87240c174e17
Sebastian Salvatore <sebastiansalvatore@gmail.com>
2015-04-03 18:59:03 -0300

    Articulos en navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :comments
+
   devise_for :users
   resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150329053647) do
+ActiveRecord::Schema.define(version: 20150403191236) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 20150329053647) do
   end
 
   add_index "articles", ["user_id"], name: "index_articles_on_user_id"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "article_id"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -39,6 +50,13 @@ ActiveRecord::Schema.define(version: 20150329053647) do
     t.string   "permission_level"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "nombre"
+    t.string   "apellido"
+    t.string   "username"
+    t.string   "oauth_token"
+    t.datetime "oauth_expired_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
